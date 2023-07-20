2bebab3e24c4953c8ae4e508294b0cbf2e433e2a
aglorice <401208941@qq.com>
2023-07-21 07:53:38 +0800

    update:更新了部分常量

--- config.py
+++ config.py
@@ -1,20 +1,23 @@
 # 爬取指定身份内的所有地区景点的评论数据
-areas = ['四川']
+AREAS = ['四川']
 
 # 爬取评论时每页的数据
-pageSize = 20
+PAGESIZE = 20
+
+# 爬取评论的页数
+MAX_PAGE = 300
 
 # 是否启动代理
-is_proxy = False
+IS_PROXY = False
 
 # 是否启动随机UA
-is_fake_user_agent = False
+IS_FAKE_USER_AGENT = False
 
 # 是否启动验证ssl
-is_verify = False
+IS_VERIFY = False
 
 # 是否要覆盖已经保存的excel文件
-is_over = False
+IS_OVER = False
 
 # 延时时间（城市）
 CITY_SLEEP_TIME = 10
@@ -27,6 +30,3 @@ POOL_NUMBER = 50
 
 # 请求超时时间
 TIME_OUT = 5
-
-# 爬取评论的页数
-MAX_PAGE = 300
